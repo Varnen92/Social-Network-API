6a5c14228d2d9f4ea7d52ea5d164490a46fe348f
Robert Sandoval <robsand1992@yahoo.com>
2022-01-31 12:14:44 -0800

    Added and completed User Model

--- models/User.js
+++ models/User.js
@@ -12,7 +12,33 @@ const UserSchema = new Schema(
             type: String,
             required: true,
             unique: true,
-            
-        }
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill out a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
     }
-)
+);
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
+
+const User = model('User', UserSchema)
+
+module.exports = User
